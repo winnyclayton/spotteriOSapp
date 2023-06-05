30bc8f455fcf3b338eef005ef2b008cf82d3d81c
winny <9036@ait.nsw.edu.au>
2023-06-05 12:08:38 +1000

    Initial Commit

---
+++ SPOTTERAPP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SPOTTERAPP
+//
+//  Created by Winona Clayton on 5/6/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
